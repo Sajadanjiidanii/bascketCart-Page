a2f24a9445dddf7f68f904348b18e3ed2f06e221
Sajadanjiidanii <154419813+Sajadanjiidanii@users.noreply.github.com>
2024-10-16 17:04:47 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const $ = document;
+let bascketCart = [];
+
+function loadcart(){
+    bascketCart.push(JSON.parse(localStorage.getItem("bascketCart")));
+}
+
+/// start append products in html ///
+
+const cartList = $.querySelector("#cartList");
+bascketCart.forEach(function(Product){
+    cartList.insertAdjacentHTML("beforeend" , "<li class=\"bascketItem\"><div><img class=\"ProductsImg\" src=\""+ Product.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ Product.name +"</p><p class=\"price\">"+ Product.pricy +" تومان</p></div></li>");
+});
+
+/// end append products in html ///
+
+function itemHover(event){
+    let contentBox = event.target;
+    contentBox.innerHTML = "<i class='bx bxs-trash'></i>";
+}
+function itemUnHover(event){
+    let contentBox = event.target.firstChild;
+    contentBox.innerHTML = "<img class=\"ProductsImg\" src=\"#\" alt=\"ProductsImg\"><p class=\"ProductsName\"> Product.name </p><p class=\"price\">تومان</p>";
+}
+function remItem(event){
+    let itemTarget = event.target.parentElement;
+    console.log(itemTarget);
+    itemTarget.style.display = "none";
+}
+
+let items = $.querySelectorAll(".bascketItem");
+items.forEach(function(item){
+    item.addEventListener("mouseenter" , itemHover);
+    item.addEventListener("mouseleave" , itemUnHover);
+    item.addEventListener("click" , remItem);
+});
+
+/// sign in chacke ///
+
+const loginSignup = $.querySelector("#login-signup");
+const logIn = $.querySelector("#login");
+const signUp = $.querySelector("#signup");
+localStorage.setItem("ShowName","Yes");
+
+function exitAccount(){
+    userAccount.style.display = "none";
+    exitAccountBtn.style.display = "none";
+    logIn.style.display = "inline";
+    signUp.style.display = "inline";
+    localStorage.setItem("ShowName","No");
+}
+
+function signChack(){
+    let isName = localStorage.getItem("Name");
+    let userName = localStorage.getItem("Name");
+    let showUser = localStorage.getItem("ShowName");
+
+    if( isName ){
+        if( showUser == "Yes"){
+            logIn.style.display = "none";
+            signUp.style.display = "none";
+            loginSignup.insertAdjacentHTML("afterbegin","<span id=\"userAccount\">" + userName + "</span><span id=\"exitAccountBtn\"> / خروج </span>");
+            const userAccount = $.querySelector("userAccount");
+            const exitAccountBtn = $.querySelector("#exitAccountBtn");
+            exitAccountBtn.addEventListener("click",exitAccount);
+        }
+    }
+};
+window.addEventListener("load",signChack);
+window.addEventListener("load",loadcart);
