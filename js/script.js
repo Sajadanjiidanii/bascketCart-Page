f454e9d829b10d32d65046587da96015b9d797ec
Sajadanjiidanii <154419813+Sajadanjiidanii@users.noreply.github.com>
2024-10-18 15:01:06 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const $ = document;
 let bascketCart = [];
+const cartList = $.getElementById("cartList");
 
 function loadcart(){
     bascketCart = JSON.parse(localStorage.getItem("bascketCart"));
+    let i = 1;
+    bascketCart.forEach(function(product){
+        product.id = i;
+        i++;
+    }); 
 
     /// start append products in html ///
 
-    const cartList = $.getElementById("cartList");
     bascketCart.forEach(function(Product){
-        cartList.insertAdjacentHTML("beforeend" , "<li class=\"bascketItem\"><i class='hide'></i><div><img class=\"ProductsImg\" src="+Product.img+" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ Product.name +"</p><p class=\"price\">"+ Product.pricy +"</p></div></li>");
+        cartList.insertAdjacentHTML("beforeend" , "<li id="+Product.id+" class=\"bascketItem\"><i class='hide'></i><div><img class=\"ProductsImg\" src="+Product.img+" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ Product.name +"</p><p class=\"price\">"+ Product.pricy +"</p></div></li>");
     });
 
     function itemHover(event){
@@ -20,12 +25,30 @@ function loadcart(){
         event.target.firstChild.className = "hide";
     }
     function remItem(event){
+        let remId = bascketCart.find(function(item){
+            return item.id == event.target.id;
+        });
+        
+        console.log(bascketCart);
+        bascketCart.forEach(function(item){
+            if(item.id == remId.id){
+                let itemIndex = bascketCart.indexOf(item);
+                bascketCart.splice(itemIndex , 1);
+            }
+        });
+        console.log(bascketCart);
+
+        localStorage.setItem("bascketCart",JSON.stringify(bascketCart))
+        localStorage.setItem("productInfo",JSON.stringify(bascketCart))
+
         let itemTarget = event.target.parentElement;
         if( itemTarget.className == "bascketItem" ){
             itemTarget.style.display = "none";
         }else{
             event.target.style.display = "none";
         }
+
+        resultProduct();
     }
     
     let items = $.querySelectorAll(".bascketItem");
@@ -34,9 +57,9 @@ function loadcart(){
         item.addEventListener("mouseleave" , itemUnHover);
         item.addEventListener("click" , remItem);
     });
-
-    /// result box code ///
-
+    resultProduct();
+}
+function resultProduct(){
     const resultPricy = $.querySelector("#resultPricy");
     let productResult = 0;
     bascketCart.forEach(function(Product){
@@ -52,6 +75,25 @@ function loadcart(){
         localStorage.removeItem("productInfo");
         location.href = "https://sajadanjiidanii.github.io/ColorShop/";
     });
+}
+
+function respons(){
+    let widthSize = window.innerWidth;
+    let rowsNumber = bascketCart.length;
+
+    if( widthSize <= 600){
+        cartList.style.gridTemplateRows = "repeat("+rowsNumber+",auto)";
+
+    }else if( widthSize <= 900){
+        rowsNumber = rowsNumber / 2;
+        cartList.style.gridTemplateColumns = "repeat(2,20%);";
+        cartList.style.gridTemplateRows = "repeat("+Math.ceil(rowsNumber)+",auto)";
+        
+    }else if( widthSize > 900){
+        rowsNumber = rowsNumber / 3;
+        cartList.style.gridTemplateColumns = "repeat(3,20%);";
+        cartList.style.gridTemplateRows = "repeat("+Math.ceil(rowsNumber)+",auto)";
+    }
 }
 
 /// sign in chacke ///
@@ -84,6 +126,9 @@ function signChack(){
             exitAccountBtn.addEventListener("click",exitAccount);
         }
     }
+
 };
 window.addEventListener("load",signChack);
 window.addEventListener("load",loadcart);
+window.addEventListener("load",respons);
+window.addEventListener("resize",respons);
